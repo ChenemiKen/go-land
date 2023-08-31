e1fc69ae1b38e45fcfa34883e753c04e547a6ebd
ChenemiKen <chenemiken15@gmail.com>
2023-09-01 00:13:44 +0100

    base endpoint

---
+++ cilian/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func main(){
+	
+
+	router := gin.Default()
+
+	router.GET("/", base)
+
+	router.Run("localhost:8020")
+}
+
+func base(c *gin.Context) {
+	fmt.Println("hello, world!")
+	c.JSON(http.StatusOK, "hello, world!")
+}
