ce5ef0b429ed1dd1b115f21c286e48a2264ca3ed
ChenemiKen <chenemiken15@gmail.com>
2023-11-21 09:34:34 +0100

    bookings init

---
+++ bookings/cmd/web/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/chenemiken/goland/bookings/pkg/config"
+	"github.com/chenemiken/goland/bookings/pkg/handlers"
+	"github.com/chenemiken/goland/bookings/pkg/render"
+)
+
+const portNumber = ":8080"
+
+var app config.AppConfig
+var session scs.SessionManager
+
+func main() {
+	app.InProduction = false
+
+	session = *scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+	app.Session = &session
+
+	render.NewTemplates(&app)
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
+
+	fmt.Printf((fmt.Sprintf("Starting application on port %s \n", portNumber)))
+	// _ = http.ListenAndServe(portNumber, nil)
+
+	server := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = server.ListenAndServe()
+	log.Fatal(err)
+}
---
+++ bookings/pkg/handlers/handlers.go
@@ -0,0 +1,42 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/chenemiken/goland/bookings/models"
+	"github.com/chenemiken/goland/bookings/pkg/config"
+	"github.com/chenemiken/goland/bookings/pkg/render"
+)
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+var Repo *Repository
+
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIp := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remoteIp", remoteIp)
+	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
+}
+
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	var stringMap = make(map[string]string)
+	stringMap["test"] = "Hello dean"
+
+	stringMap["remoteIp"] = m.App.Session.GetString(r.Context(), "remoteIp")
+
+	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
