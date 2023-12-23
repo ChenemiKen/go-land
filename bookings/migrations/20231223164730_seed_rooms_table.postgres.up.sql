86b00b3660f6a765ad956c769593d4aab0c12f1b
ChenemiKen <chenemiken15@gmail.com>
2023-12-23 18:14:30 +0100

    added migrations to seed rooms and restrictions

---
+++ bookings/migrations/20231223164730_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+insert into rooms (id, room_name, created_at, updated_at) values
+(1, 'general''s quarters', '2023-12-18 00:00:00',	'2023-12-18 00:00:00'),
+(2,	'major''s suite',	'2023-12-18 00:00:00',	'2023-12-18 00:00:00');
---
+++ bookings/migrations/20231223170744_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+insert into restrictions (id,	restriction_name,	created_at,	updated_at) values
+(1,	'reservation',	'2023-12-18 00:00:00',	'2023-12-18 00:00:00'),
+(2,	'owner_block',	'2023-12-19 00:00:00',	'2023-12-19 00:00:00');
